2666bf19f97f47c7167796470650382ecc920f38
mhi@thansen.dk <mhi@thansen.dk>
2023-07-13 15:31:04 +0200

    More styling

--- src/components/ToDo/TableList.vue
+++ src/components/ToDo/TableList.vue
@@ -1,9 +1,11 @@
 <template>
   <div
-    class="relative flex overflow-x-auto shadow-md sm:rounded-lg"
+    class="relative flex max-w-xs overflow-x-auto shadow-md sm:rounded-lg"
     v-if="drills.length >= 1"
   >
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table
+      class="w-full max-w-xs text-sm text-left text-gray-500 dark:text-gray-400"
+    >
       <thead
         class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
       >
