70c0a7fa86fd77de4c25c3ef4a9f86f01387dfea
mhi@thansen.dk <mhi@thansen.dk>
2023-07-14 14:50:32 +0200

    Added textarea instead

--- src/components/ToDo/EditItem.vue
+++ src/components/ToDo/EditItem.vue
@@ -42,30 +42,30 @@
             >
               <div class="w-full">
                 <label
-                  for="price"
+                  for="description"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >Øvelse</label
+                  >Description</label
                 >
-                <input
-                  type="text"
-                  name="price"
-                  id="price"
-                  :value="drill.name"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                />
+                <textarea
+                  id="description"
+                  rows="8"
+                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Your description here"
+                  v-model="drill.name"
+                ></textarea>
 
                 <label
-                  for="price"
+                  for="description"
                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >Beskrivelse</label
+                  >Description</label
                 >
-                <input
-                  type="text"
-                  name="price"
-                  id="price"
-                  :value="drill.description"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                />
+                <textarea
+                  id="description"
+                  rows="8"
+                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Your description here"
+                  v-model="drill.description"
+                ></textarea>
               </div>
               <div class="flex justify-between">
                 <div class="flex items-start">
