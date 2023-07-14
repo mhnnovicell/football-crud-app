334f592a9746de2d6efd485b8fcad52423c5d8d6
mhi@thansen.dk <mhi@thansen.dk>
2023-07-14 11:38:00 +0200

    Changed opacity for backdrop

--- src/components/ToDo/EditItem.vue
+++ src/components/ToDo/EditItem.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Main modal -->
-  <div class="fixed inset-0 z-40 bg-black opacity-50"></div>
+  <div class="fixed inset-0 z-40 bg-black opacity-75"></div>
   <div
     tabindex="-1"
     class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
