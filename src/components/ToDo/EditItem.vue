58530f44a2ed5f713d9faeaa0c03a5460f164ec3
mhi@thansen.dk <mhi@thansen.dk>
2023-07-14 11:49:35 +0200

    Added the possibility to set active state on item

--- src/components/ToDo/EditItem.vue
+++ src/components/ToDo/EditItem.vue
@@ -35,32 +35,65 @@
             Sæt øvelse som aktiv
           </h3>
           <form class="space-y-6" action="#">
-            <div class="flex justify-between">
-              <div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    value=""
-                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                    v-model="isActive"
-                  />
-                </div>
-                <label
-                  for="remember"
-                  class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >Aktiv</label
-                >
-              </div>
-            </div>
-            <button
-              @click="todoStore.editDrill(isActive)"
-              type="submit"
-              class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            <div
+              class="flex flex-col w-full"
+              v-for="drill in todoStore.drills"
+              :key="drill.id"
             >
-              Gem
-            </button>
+              <div class="w-full">
+                <label
+                  for="price"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Øvelse</label
+                >
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  :value="drill.name"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                />
+
+                <label
+                  for="price"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Beskrivelse</label
+                >
+                <input
+                  type="text"
+                  name="price"
+                  id="price"
+                  :value="drill.description"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                />
+              </div>
+              <div class="flex justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="remember"
+                      type="checkbox"
+                      value=""
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                      required
+                      v-model="isActive"
+                    />
+                  </div>
+                  <label
+                    for="remember"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >Aktiv</label
+                  >
+                </div>
+              </div>
+              <button
+                @click.prevent="todoStore.editDrill(isActive, drill.id)"
+                type="submit"
+                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Gem
+              </button>
+            </div>
           </form>
         </div>
       </div>
